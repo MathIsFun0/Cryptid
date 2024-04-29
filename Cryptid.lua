894ae68dd46e7c49f958573e2050cdae4f0d29b2
lmesh91 <89877664+MathIsFun0@users.noreply.github.com>
2024-04-29 08:39:55 -0400

    Gateway (obtaining Exotics)

--- Cryptid.lua
+++ Cryptid.lua
@@ -280,6 +280,36 @@ function SMODS.INIT.Cryptid()
         end
     end
 
+    -- Exotic color
+    function loc_colour(_c, _default)
+        G.ARGS.LOC_COLOURS = G.ARGS.LOC_COLOURS or {
+          red = G.C.RED,
+          mult = G.C.MULT,
+          blue = G.C.BLUE,
+          chips = G.C.CHIPS,
+          green = G.C.GREEN,
+          money = G.C.MONEY,
+          gold = G.C.GOLD,
+          attention = G.C.FILTER,
+          purple = G.C.PURPLE,
+          white = G.C.WHITE,
+          inactive = G.C.UI.TEXT_INACTIVE,
+          spades = G.C.SUITS.Spades,
+          hearts = G.C.SUITS.Hearts,
+          clubs = G.C.SUITS.Clubs,
+          diamonds = G.C.SUITS.Diamonds,
+          tarot = G.C.SECONDARY_SET.Tarot,
+          planet = G.C.SECONDARY_SET.Planet,
+          spectral = G.C.SECONDARY_SET.Spectral,
+          edition = G.C.EDITION,
+          dark_edition = G.C.DARK_EDITION,
+          legendary = G.C.RARITY[4],
+          exotic = G.C.RARITY["Exotic"],
+          enhanced = G.C.SECONDARY_SET.Enhanced
+        }
+        return G.ARGS.LOC_COLOURS[_c] or _default or G.C.UI.TEXT_DARK
+      end
+
     -- File loading based on Relic-Jokers
     local files = NFS.getDirectoryItems(SMODS.findModByID("Cryptid").path.."Items")
     for _, file in ipairs(files) do
