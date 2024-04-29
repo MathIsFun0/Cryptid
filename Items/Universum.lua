f9a156577ac0cf7d69453df06b3e98ed57ec00ce
lmesh91 <89877664+MathIsFun0@users.noreply.github.com>
2024-04-29 10:28:13 -0400

    use Lovely instead of overriding

--- Items/Universum.lua
+++ Items/Universum.lua
@@ -1,9 +1,7 @@
-local universum_extra = 2
-
 local universum = SMODS.Joker:new(
 	"Universum", --name
 	"cry_universum", --slug
-	{extra = universum_extra}, --config
+	{extra = 2}, --config
 	{x = 0, y = 0}, --spritePos
 	{
         name = 'Universum',
@@ -23,45 +21,6 @@ local universum = SMODS.Joker:new(
 	'j_cry_universum', --atlas
 	{x = 2, y = 0, extra = {x = 1, y = 0}} --soul_pos
 )
-
--- It's easier to not override this, actually
-function level_up_hand(card, hand, instant, amount)
-    if next(find_joker("Universum")) then
-        amount = amount or 1
-        G.GAME.hands[hand].level = math.max(0, G.GAME.hands[hand].level + amount)
-        G.GAME.hands[hand].mult = math.max(G.GAME.hands[hand].mult * math.pow(universum_extra, amount), 1)
-        G.GAME.hands[hand].chips = math.max(G.GAME.hands[hand].chips * math.pow(universum_extra, amount), 0)
-    else
-        amount = amount or 1
-        G.GAME.hands[hand].level = math.max(0, G.GAME.hands[hand].level + amount)
-        G.GAME.hands[hand].mult = math.max(G.GAME.hands[hand].mult + G.GAME.hands[hand].l_mult*amount, 1)
-        G.GAME.hands[hand].chips = math.max(G.GAME.hands[hand].chips + G.GAME.hands[hand].l_chips*amount, 0)
-    end
-    if not instant then 
-        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.2, func = function()
-            play_sound('tarot1')
-            if card then card:juice_up(0.8, 0.5) end
-            G.TAROT_INTERRUPT_PULSE = true
-            return true end }))
-        update_hand_text({delay = 0}, {mult = G.GAME.hands[hand].mult, StatusText = true})
-        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.9, func = function()
-            play_sound('tarot1')
-            if card then card:juice_up(0.8, 0.5) end
-            return true end }))
-        update_hand_text({delay = 0}, {chips = G.GAME.hands[hand].chips, StatusText = true})
-        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.9, func = function()
-            play_sound('tarot1')
-            if card then card:juice_up(0.8, 0.5) end
-            G.TAROT_INTERRUPT_PULSE = nil
-            return true end }))
-        update_hand_text({sound = 'button', volume = 0.7, pitch = 0.9, delay = 0}, {level=G.GAME.hands[hand].level})
-        delay(1.3)
-    end
-    G.E_MANAGER:add_event(Event({
-        trigger = 'immediate',
-        func = (function() check_for_unlock{type = 'upgrade_hand', hand = hand, level = G.GAME.hands[hand].level} return true end)
-    }))
-end
 
 local uht = update_hand_text
 function update_hand_text(config, vals)
