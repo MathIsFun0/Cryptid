675adc2abd70cb12196ab5d463bbf2d303fccc40
lmesh91 <89877664+MathIsFun0@users.noreply.github.com>
2024-05-04 09:26:00 -0400

    iterum

--- Cryptid.lua
+++ Cryptid.lua
@@ -1,6 +1,7 @@
 --- STEAMODDED HEADER
 --- MOD_NAME: Cryptid
 --- MOD_ID: Cryptid
+--- PREFIX: cry
 --- MOD_AUTHOR: [MathIsFun_, Balatro Discord]
 --- MOD_DESCRIPTION: Adds unbalanced ideas to Balatro.
 --- BADGE_COLOUR: 708b91
@@ -10,53 +11,46 @@
 
 _RELEASE_MODE = false
 
-function SMODS.INIT.Cryptid()
-    -- Custom Rarity setup (based on Relic-Jokers)
-	G.localization.misc.dictionary.k_cry_exotic = "Exotic"
-    Game:set_globals()
-    G.C.RARITY["Exotic"] = HEX("708b91");
-    local SMODS_inject_jokers_ref = SMODS.injectJokers
-	function SMODS.injectJokers()
-		G.P_JOKER_RARITY_POOLS["Exotic"] = {}
-		SMODS_inject_jokers_ref()
-	end
-    local get_badge_colourref = get_badge_colour
-	function get_badge_colour(key)
-		local fromRef = get_badge_colourref(key)
-	
-		if key == 'cry_exotic' then return G.C.RARITY["Exotic"]
-		else return fromRef end
-	end
+-- Custom Rarity setup (based on Relic-Jokers)
+Game:set_globals()
+G.C.RARITY["Exotic"] = HEX("708b91");
+G.P_JOKER_RARITY_POOLS["Exotic"] = {}
+local get_badge_colourref = get_badge_colour
+function get_badge_colour(key)
+    local fromRef = get_badge_colourref(key)
 
-    -- Midground sprites
-    local set_spritesref = Card.set_sprites
-    function Card:set_sprites(_center, _front)
-        set_spritesref(self, _center, _front);
-        if _center and _center.name == 'Gateway' then
-            self.children.floating_sprite = Sprite(self.T.x, self.T.y, self.T.w, self.T.h, G.ASSET_ATLAS[_center.atlas or _center.set], {x=2,y=0})
-            self.children.floating_sprite.role.draw_major = self
-            self.children.floating_sprite.states.hover.can = false
-            self.children.floating_sprite.states.click.can = false
-            self.children.floating_sprite2 = Sprite(self.T.x, self.T.y, self.T.w, self.T.h, G.ASSET_ATLAS[_center.atlas or _center.set], {x=1,y=0})
-            self.children.floating_sprite2.role.draw_major = self
-            self.children.floating_sprite2.states.hover.can = false
-            self.children.floating_sprite2.states.click.can = false
-        end
-        if _center and _center.soul_pos and _center.soul_pos.extra then
-            self.children.floating_sprite2 = Sprite(self.T.x, self.T.y, self.T.w, self.T.h, G.ASSET_ATLAS[_center.atlas or _center.set], _center.soul_pos.extra)
-            self.children.floating_sprite2.role.draw_major = self
-            self.children.floating_sprite2.states.hover.can = false
-            self.children.floating_sprite2.states.click.can = false
-        end
+    if key == 'cry_exotic' then return G.C.RARITY["Exotic"]
+    else return fromRef end
+end
+
+-- Midground sprites
+local set_spritesref = Card.set_sprites
+function Card:set_sprites(_center, _front)
+    set_spritesref(self, _center, _front);
+    if _center and _center.name == 'Gateway' then
+        self.children.floating_sprite = Sprite(self.T.x, self.T.y, self.T.w, self.T.h, G.ASSET_ATLAS[_center.atlas or _center.set], {x=2,y=0})
+        self.children.floating_sprite.role.draw_major = self
+        self.children.floating_sprite.states.hover.can = false
+        self.children.floating_sprite.states.click.can = false
+        self.children.floating_sprite2 = Sprite(self.T.x, self.T.y, self.T.w, self.T.h, G.ASSET_ATLAS[_center.atlas or _center.set], {x=1,y=0})
+        self.children.floating_sprite2.role.draw_major = self
+        self.children.floating_sprite2.states.hover.can = false
+        self.children.floating_sprite2.states.click.can = false
     end
+    if _center and _center.soul_pos and _center.soul_pos.extra then
+        self.children.floating_sprite2 = Sprite(self.T.x, self.T.y, self.T.w, self.T.h, G.ASSET_ATLAS[_center.atlas or _center.set], _center.soul_pos.extra)
+        self.children.floating_sprite2.role.draw_major = self
+        self.children.floating_sprite2.states.hover.can = false
+        self.children.floating_sprite2.states.click.can = false
+    end
+end
 
-    -- File loading based on Relic-Jokers
-    local files = NFS.getDirectoryItems(SMODS.findModByID("Cryptid").path.."Items")
-    for _, file in ipairs(files) do
-        local curr_obj = NFS.load(SMODS.findModByID("Cryptid").path.."Items/"..file)()
-        for _, item in ipairs(curr_obj) do
-            item:register()
-        end
+-- File loading based on Relic-Jokers
+local files = NFS.getDirectoryItems(SMODS.current_mod.path.."Items")
+for _, file in ipairs(files) do
+    local curr_obj = NFS.load(SMODS.current_mod.path.."Items/"..file)()
+    for _, item in ipairs(curr_obj) do
+        item:register()
     end
 end
 ----------------------------------------------
