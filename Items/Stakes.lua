4188a5d6a5bcbe65709b60e82cf56994b3f4eeac
lmesh91 <89877664+MathIsFun0@users.noreply.github.com>
2024-05-08 00:53:23 -0400

    metadata for stakes 17-24

--- Items/Stakes.lua
+++ Items/Stakes.lua
@@ -125,6 +125,140 @@ local diamond = SMODS.Stake({
     shiny = true,
     color = HEX("88e5d9")
 })
+local amber = SMODS.Stake({
+	name = "Amber Stake",
+	key = "amber",
+	pos = {x = 3, y = 1},
+    atlas = "stake",
+    applied_stakes = {"cry_diamond"},
+	loc_txt = {
+        name = "Amber Stake",
+        text = {
+        "{C:attention}-1{} Booster Pack slot",
+        "{s:0.8,C:inactive}(Not yet implemented){}",
+        }
+    },
+    shiny = true,
+    color = HEX("feb900")
+})
+local bronze = SMODS.Stake({
+	name = "Bronze Stake",
+	key = "bronze",
+	pos = {x = 4, y = 1},
+    atlas = "stake",
+    applied_stakes = {"cry_amber"},
+	loc_txt = {
+        name = "Bronze Stake",
+        text = {
+        "Vouchers are {C:attention}50%{} more expensive",
+        "{s:0.8,C:inactive}(Not yet implemented){}",
+        }
+    },
+    shiny = true,
+    color = HEX("d27c37")
+})
+local quartz = SMODS.Stake({
+	name = "Quartz Stake",
+	key = "quartz",
+	pos = {x = 0, y = 2},
+    atlas = "stake",
+    applied_stakes = {"cry_bronze"},
+	loc_txt = {
+        name = "Quartz Stake",
+        text = {
+        "Jokers can be {C:attention}Pinned{}",
+        "{s:0.8,C:inactive}(Stays pinned to the leftmost position){}",
+        "{s:0.8,C:inactive}(Not yet implemented){}",
+        }
+    },
+    shiny = true,
+    color = HEX("e8e8e8")
+})
+local ruby = SMODS.Stake({
+	name = "Ruby Stake",
+	key = "ruby",
+	pos = {x = 1, y = 2},
+    atlas = "stake",
+    applied_stakes = {"cry_quartz"},
+	loc_txt = {
+        name = "Ruby Stake",
+        text = {
+        "{C:attention}Big{} Blinds can become",
+        "{C:attention}Boss{} Blinds",
+        "{s:0.8,C:inactive}(Not yet implemented){}",
+        }
+    },
+    shiny = true,
+    color = HEX("fc5f55")
+})
+local topaz = SMODS.Stake({
+	name = "Topaz Stake",
+	key = "topaz",
+	pos = {x = 2, y = 2},
+    atlas = "stake",
+    applied_stakes = {"cry_ruby"},
+	loc_txt = {
+        name = "Topaz Stake",
+        text = {
+        "Blind rewards decreased by {C:attention}$2{}",
+        "{s:0.8,C:inactive}(Not yet implemented){}",
+        }
+    },
+    shiny = true,
+    color = HEX("fcbb63")
+})
+local sapphire = SMODS.Stake({
+	name = "Sapphire Stake",
+	key = "sapphire",
+	pos = {x = 3, y = 2},
+    atlas = "stake",
+    applied_stakes = {"cry_topaz"},
+	loc_txt = {
+        name = "Sapphire Stake",
+        text = {
+        "Lose {C:attention}25%{} of current money",
+        "at end of Ante",
+        "{s:0.8,C:inactive}(Up to $10){}",
+        "{s:0.8,C:inactive}(Not yet implemented){}",
+        }
+    },
+    shiny = true,
+    color = HEX("3551fc")
+})
+local emerald = SMODS.Stake({
+	name = "Emerald Stake",
+	key = "emerald",
+	pos = {x = 4, y = 2},
+    atlas = "stake",
+    applied_stakes = {"cry_sapphire"},
+	loc_txt = {
+        name = "Emerald Stake",
+        text = {
+        "Cards, packs, and vouchers",
+        "can be {C:attention}face down{}",
+        "{s:0.8,C:inactive}(Unable to be viewed until purchased){}",
+        "{s:0.8,C:inactive}(Not yet implemented){}",
+        }
+    },
+    shiny = true,
+    color = HEX("06fc2c")
+})
+local platinum = SMODS.Stake({
+	name = "Platinum Stake",
+	key = "platinum",
+	pos = {x = 0, y = 3},
+    atlas = "stake",
+    applied_stakes = {"cry_emerald"},
+	loc_txt = {
+        name = "Platinum Stake",
+        text = {
+        "Small Blinds are {C:attention}removed{}",
+        "{s:0.8,C:inactive}(Not yet implemented){}",
+        }
+    },
+    shiny = true,
+    color = HEX("b0f6ff")
+})
 
 local stake_atlas = SMODS.Sprite({
     key = "stake",
@@ -134,4 +268,5 @@ local stake_atlas = SMODS.Sprite({
     py = 29
 })
 
-return {stake_atlas, pink, brown, yellow, silver, cyan, gray, crimson, diamond}
+return {stake_atlas, pink, brown, yellow, silver, cyan, gray, crimson, diamond,
+        amber, bronze, quartz, ruby, topaz, sapphire, emerald, platinum}
