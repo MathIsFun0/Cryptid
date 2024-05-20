2866d0a7180c0b21e1aa470c35ded4c2f3ee1a07
lmesh91 <89877664+MathIsFun0@users.noreply.github.com>
2024-05-19 23:56:38 -0400

    Update Cryptid.lua

--- Cryptid.lua
+++ Cryptid.lua
@@ -169,6 +169,101 @@ function create_UIBox_settings()
     return t
   end
 
+-- We're modifying so much of this for Brown and Yellow Stake, Equilibrium Deck, etc. that it's fine to override...
+function create_card(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
+  local area = area or G.jokers
+  local center = G.P_CENTERS.b_red
+      
+
+  --should pool be skipped with a forced key
+  if not forced_key and soulable and (not G.GAME.banned_keys['c_soul']) then
+      for _, v in ipairs(SMODS.Consumable.legendaries) do
+          if (_type == v.type.key or _type == v.soul_set) and not (G.GAME.used_jokers[v.key] and not next(find_joker("Showman")) and not v.can_repeat_soul) then
+              if pseudorandom('soul_'..v.key.._type..G.GAME.round_resets.ante) > (1 - v.soul_rate) then
+                  forced_key = v.key
+              end
+          end
+      end
+          if (_type == 'Tarot' or _type == 'Spectral' or _type == 'Tarot_Planet') and
+      not (G.GAME.used_jokers['c_soul'] and not next(find_joker("Showman")))  then
+          if pseudorandom('soul_'.._type..G.GAME.round_resets.ante) > 0.997 then
+              forced_key = 'c_soul'
+          end
+      end
+      if (_type == 'Planet' or _type == 'Spectral') and
+      not (G.GAME.used_jokers['c_black_hole'] and not next(find_joker("Showman")))  then 
+          if pseudorandom('soul_'.._type..G.GAME.round_resets.ante) > 0.997 then
+              forced_key = 'c_black_hole'
+          end
+      end
+  end
+
+  if _type == 'Base' then 
+      forced_key = 'c_base'
+  end
+
+
+
+  if forced_key and not G.GAME.banned_keys[forced_key] then 
+      center = G.P_CENTERS[forced_key]
+      _type = (center.set ~= 'Default' and center.set or _type)
+  else
+      local _pool, _pool_key = get_current_pool(_type, _rarity, legendary, key_append)
+      center = pseudorandom_element(_pool, pseudoseed(_pool_key))
+      local it = 1
+      while center == 'UNAVAILABLE' do
+          it = it + 1
+          center = pseudorandom_element(_pool, pseudoseed(_pool_key..'_resample'..it))
+      end
+
+      center = G.P_CENTERS[center]
+  end
+
+  local front = ((_type=='Base' or _type == 'Enhanced') and pseudorandom_element(G.P_CARDS, pseudoseed('front'..(key_append or '')..G.GAME.round_resets.ante))) or nil
+
+  local card = Card(area.T.x + area.T.w/2, area.T.y, G.CARD_W*(center and center.set == 'Booster' and 1.27 or 1), G.CARD_H*(center and center.set == 'Booster' and 1.27 or 1), front, center,
+  {bypass_discovery_center = area==G.shop_jokers or area == G.pack_cards or area == G.shop_vouchers or (G.shop_demo and area==G.shop_demo) or area==G.jokers or area==G.consumeables,
+  bypass_discovery_ui = area==G.shop_jokers or area == G.pack_cards or area==G.shop_vouchers or (G.shop_demo and area==G.shop_demo),
+  discover = area==G.jokers or area==G.consumeables, 
+  bypass_back = G.GAME.selected_back.pos})
+  if card.ability.consumeable and not skip_materialize then card:start_materialize() end
+
+  if _type == 'Joker' or G.GAME.modifiers.cry_any_stickers then
+      if G.GAME.modifiers.all_eternal then
+          card:set_eternal(true)
+      end
+      if (area == G.shop_jokers) or (area == G.pack_cards) then 
+          local eternal_perishable_poll = pseudorandom('cry_et'..(key_append or '')..G.GAME.round_resets.ante)
+          if G.GAME.modifiers.enable_eternals_in_shop and eternal_perishable_poll > 0.7 then
+              card:set_eternal(true)
+          end
+          if G.GAME.modifiers.enable_perishables_in_shop then
+              if not G.GAME.modifiers.cry_eternal_perishable_compat and ((eternal_perishable_poll > 0.4) and (eternal_perishable_poll <= 0.7)) then
+                  card:set_perishable(true)
+              end
+              if G.GAME.modifiers.cry_eternal_perishable_compat and pseudorandom('cry_per'..(key_append or '')..G.GAME.round_resets.ante) > 0.7 then
+                  card:set_perishable(true)
+              end
+          end
+          if G.GAME.modifiers.enable_rentals_in_shop and pseudorandom('cry_ssjr'..(key_append or '')..G.GAME.round_resets.ante) > 0.7 then
+              card:set_rental(true)
+          end
+          if G.GAME.modifiers.cry_enable_pinned_in_shop and pseudorandom('cry_pin'..(key_append or '')..G.GAME.round_resets.ante) > 0.7 then
+              card.pinned = true
+          end
+          if G.GAME.modifiers.cry_enable_flipped_in_shop and pseudorandom('cry_flip'..(key_append or '')..G.GAME.round_resets.ante) > 0.7 then
+              card.cry_flipped = true
+          end
+      end
+      if _type == 'Joker' then
+          local edition = poll_edition('edi'..(key_append or '')..G.GAME.round_resets.ante)
+          card:set_edition(edition)
+          check_for_unlock({type = 'have_edition'})
+      end
+  end
+  return card
+end
+
 SMODS.Atlas({
     key = "modicon",
     path = "cry_icon.png",
