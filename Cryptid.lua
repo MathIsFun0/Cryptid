24b9b9980f745df6f7f17c7c5f0fdffb135e3679
lmesh91 <89877664+MathIsFun0@users.noreply.github.com>
2024-06-01 17:44:51 -0400

    bump version

--- Cryptid.lua
+++ Cryptid.lua
@@ -6,7 +6,7 @@
 --- MOD_DESCRIPTION: Adds unbalanced ideas to Balatro.
 --- BADGE_COLOUR: 708b91
 --- DEPENDENCIES: [Talisman]
---- VERSION: 0.3.1f
+--- VERSION: 0.3.1g
 
 ----------------------------------------------
 ------------MOD CODE -------------------------
