9343aad6c8912038b30a297f1f84bce9d589bb0e
Jevonnissocoolman <jevonnsivels2@gmail.com>
2024-06-07 20:47:08 -0700

    Update test.lua

--- Items/test.lua
+++ Items/test.lua
@@ -1,1 +1,36 @@
-
+local cursor = {
+    object_type = "Joker",
+	name = "cry-Cursor",
+	key = "cursor",
+    config = {extra = {chips = 0, chip_mod = 5}},
+	pos = {x = 0, y = 0},
+	loc_txt = {
+        name = 'Cursor',
+        text = {
+            "This Joker gains {C:chips}+#2#{} Chips",
+            "per card {C:attention}purchased{}",
+            "{C:inactive}(Currently {C:chips}+#1#{C:inactive} Chips)"
+        }
+    },
+	rarity = 1,
+	cost = 5,
+	discovered = true,
+    blueprint_compat = true,
+	atlas = "cursor",
+    loc_vars = function(self, info_queue, center)
+        return {vars = {center.ability.extra.chips, center.ability.extra.chip_mod}}
+    end,
+    calculate = function(self, card, context)
+        if context.buying_card and not context.blueprint then
+            card.ability.extra.chips = card.ability.extra.chips + card.ability.extra.chip_mod
+            card_eval_status_text(card, 'extra', nil, nil, nil, {message = localize{type = 'variable', key = 'a_chips', vars = {card.ability.extra.chips}}, colour = G.C.CHIPS})
+            return {calculated = true}
+        end
+        if context.cardarea == G.jokers and (card.ability.extra.chips > 0) and not context.before and not context.after then
+            return {
+                message = localize{type='variable',key='a_chips',vars={card.ability.extra.chips}},
+                chip_mod = card.ability.extra.chips
+            }
+        end
+    end
+}
