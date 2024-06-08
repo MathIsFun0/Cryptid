95abae0c0b349bd358169746962a7e112e1a6805
Jevonnissocoolman <jevonnsivels2@gmail.com>
2024-06-07 20:48:09 -0700

    Update test.lua

--- Items/test.lua
+++ Items/test.lua
@@ -34,3 +34,57 @@ local cursor = {
         end
     end
 }
+local jimball = {
+	object_type = "Joker",
+	name = "cry-Jimball",
+	key = "jimball",
+	pos = {x = 0, y = 0},
+    config = {x_mult = 1, extra = 0.15},
+	loc_txt = {
+        name = 'Jimball',
+        text = {
+            "This Joker gains {X:mult,C:white} X#1# {} Mult",
+            "per {C:attention}consecutive{} hand played",
+            "while playing your",
+            "most played {C:attention}poker hand",
+            "{C:inactive}(Currently {X:mult,C:white} X#2# {C:inactive} Mult)"
+		}
+    },
+	loc_vars = function(self, info_queue, center)
+		return {vars = {center.ability.extra, center.ability.x_mult}}
+    end,
+	rarity = 3,
+	cost = 10,
+	discovered = true,
+	blueprint_compat = true,
+	calculate = function(self, card, context)
+        if context.before and not context.blueprint then
+            local reset = false
+            local play_more_than = (G.GAME.hands[context.scoring_name].played or 0)
+            for k, v in pairs(G.GAME.hands) do
+                if k ~= context.scoring_name and v.played >= play_more_than and v.visible then
+                    reset = true
+                end
+            end
+            if reset then
+                if card.ability.x_mult > 1 then
+                    card.ability.x_mult = 1
+                    return {
+                        card = self,
+                        message = localize('k_reset')
+                    }
+                end
+            else
+                card.ability.x_mult = card.ability.x_mult + card.ability.extra
+            end
+        end
+	end,
+	atlas = "jimball",
+}
+local jimball_sprite = {
+	object_type = "Atlas",
+    key = "jimball",
+    path = "j_cry_jimball.png",
+    px = 71,
+    py = 95
+}
