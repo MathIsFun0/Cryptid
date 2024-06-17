c62a582b9214caee6bed551d1bd0553d83b0f68c
Jevonnissocoolman <jevonnsivels2@gmail.com>
2024-06-17 07:59:52 -0700

    Update Test.lua

--- Items/Test.lua
+++ Items/Test.lua
@@ -1,47 +1,29 @@
-if context.selling_self and card.ability.extra.active and not context.retrigger_joker and not context.blueprint then
-local potofjokes = {
+local oldcandy = {
     object_type = "Joker",
-	name = "cry-Pot of Jokes",
-	key = "pot_of_jokes",
-	config = {extra = {h_size = -2, h_mod = 1}},
+	name = "cry-oldcandy",
+	key = "oldcandy",
+	config = {extra = {h_mod = 1},
 	pos = {x = 0, y = 0},
 	loc_txt = {
-        name = 'Pot of Jokes',
+        name = 'Nostalgic Candy',
         text = {
-            "{C:attention}#1#{} hand size,",
-            "increases by",
-            "{C:blue}#2#{} every round"}
-    },
-	rarity = 3,
-	cost = 10,
-    discovered = true,
-	atlas = 'pot_of_jokes',
-    loc_vars = function(self, info_queue, center)
-        return {vars = {center.ability.extra.h_size<0 and center.ability.extra.h_size or "+"..center.ability.extra.h_size,center.ability.extra.h_mod}}
-    end,
-    calculate = function(self, card, context)
-        if context.end_of_round and not context.individual and not context.repetition and not context.blueprint then
-            card.ability.extra.h_size = card.ability.extra.h_size + card.ability.extra.h_mod
+            "Increase hand size by {C:attention}#1#{} when sold,",
+			},
+	},
+	rarity = "cry_epic",
+	cost = 6,
+	discovered = true,
+	atlas = 'oldcandy',
+	calculate = function(self, card, context)
+	if context.selling_self and not context.retrigger_joker and not context.blueprint then
             G.hand:change_size(card.ability.extra.h_mod)
-            return {
-                message = localize{type='variable',key='a_handsize',vars={card.ability.extra.h_mod}},
-                colour = G.C.FILTER,
-                card = card
-            }
         end
-    end,
-    add_to_deck = function(self, card, from_debuff)
-        G.hand:change_size(card.ability.extra.h_size)
-    end,
-    remove_from_deck = function(self, card, from_debuff)
-        G.hand:change_size(-card.ability.extra.h_size)
-    end
-}
-local potofjokes_sprite = {
+end,
+local oldcandy = {
     object_type = "Atlas",
-    key = "pot_of_jokes",
+    key = "oldcandy",
     
-    path = "j_cry_pot_of_jokes.png",
+    path = "j_cry_oldcandy.png",
     px = 71,
     py = 95
 }
