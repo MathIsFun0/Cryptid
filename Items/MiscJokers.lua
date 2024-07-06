6cb418f161a4b712deab01528750e53d45511225
Jevonnissocoolman <jevonnsivels2@gmail.com>
2024-07-06 15:21:02 -0700

    Update MiscJokers.lua

--- Items/MiscJokers.lua
+++ Items/MiscJokers.lua
@@ -1754,7 +1754,7 @@ local meteor = {
     cost = 6,
     blueprint_compat = true,
     calculate = function(self, card, context)
-        if context.other_joker and context.other_joker.ability.edition == "Foil" then
+        if context.other_joker and context.other_joker.edition.foil == true then
             if not Talisman.config_file.disable_anims then 
                 G.E_MANAGER:add_event(Event({
                     func = function()
@@ -1792,7 +1792,7 @@ local exoplanet = {
 	cost = 6,
 	blueprint_compat = true,
 	calculate = function(self, card, context)
-        if context.other_joker and context.other_joker.ability.edition == "Holographic" then
+        if context.other_joker and context.other_joker.editon.holo == true then
             if not Talisman.config_file.disable_anims then 
                 G.E_MANAGER:add_event(Event({
                     func = function()
@@ -1830,7 +1830,7 @@ local stardust = {
 	cost = 6,
 	blueprint_compat = true,
 	calculate = function(self, card, context)
-        if context.other_joker and context.other_joker.ability.edition == "Polychrome" then
+        if context.other_joker and context.other_joker.edition.polychrome == true then
             if not Talisman.config_file.disable_anims then 
                 G.E_MANAGER:add_event(Event({
                     func = function()
