41fb41404836de9d0862a8b26360a32059b7c791
Jevonnissocoolman <jevonnsivels2@gmail.com>
2024-08-01 08:33:29 -0700

    bruh

--- Items/MiscJokers.lua
+++ Items/MiscJokers.lua
@@ -2368,11 +2368,11 @@ local happy = {
                     end}))   
                     card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = localize('k_plus_joker'), colour = G.C.BLUE})
         end
-	if context.end_of_round and #G.jokers.cards + G.GAME.joker_buffer < G.jokers.config.card_limit and not context.retrigger_joker then
+	if context.end_of_round and not context.individual and #G.jokers.cards + G.GAME.joker_buffer < G.jokers.config.card_limit and not context.retrigger_joker then
     		local roundcreatejoker = math.min(1, G.jokers.config.card_limit - (#G.jokers.cards + G.GAME.joker_buffer))
     		local roundcheck = false
-   		G.GAME.joker_buffer = G.GAME.joker_buffer + roundcreatejoker --This is actually making me go insane why is this creating more than one joker
-    		G.E_MANAGER:add_event(Event({ --asdfghaskjklashdhdeflihkdhj
+   		G.GAME.joker_buffer = G.GAME.joker_buffer + roundcreatejoker 
+    		G.E_MANAGER:add_event(Event({ 
         		func = function()
             		if not roundcheck and roundcreatejoker > 0 then
                 		local card = create_card('Joker', G.jokers, nil, nil, nil, nil, nil, 'happy')
