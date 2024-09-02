9579828fdf5b3c9b40ff32932c0ef724e7783f8f
toneblock <55140928+Toneblock@users.noreply.github.com>
2024-09-02 21:36:19 +0400

    config option (off by default), final tweaks

--- https/thread.lua
+++ https/thread.lua
@@ -8,14 +8,23 @@ local script_dir = script_path:match("(.*/)")
 package.path = script_dir .. "?.lua;" .. package.path
 package.cpath = script_dir .. "?.so;" .. package.cpath
 
-local https = require("https")
+local index_os = love.system.getOS()
+
+if index_os == 'OS X' then
+	loc_https = require("macos-https")
+elseif index_os == 'Linux' then
+	loc_https = require("linux-https")
+else
+	loc_https = require("https")
+end
+
 local last_update_time = 0
 local initial = true
 while true do
 	if (os.time() - last_update_time >= 60) or initial then
 		initial = nil
 		last_update_time = os.time()
-		local resp, txt = https.request("https://gist.githubusercontent.com/Toneblock/7478d96bcf04e3b470b23d85c98e6a8c/raw/text.txt".."?v=" .. tostring(os.time()))
+		local resp, txt = loc_https.request("https://gist.githubusercontent.com/Toneblock/7478d96bcf04e3b470b23d85c98e6a8c/raw/text.txt".."?v=" .. tostring(os.time()))
 		if resp == 200 then
 			love.thread.getChannel('member_count'):push(tonumber(txt))
 		else
