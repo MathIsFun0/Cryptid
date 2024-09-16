2c3f422930ee6f2fa129275fa863238361f37793
Jevonnissocoolman <jevonnsivels2@gmail.com>
2024-09-15 22:09:47 -0700

    Ace Aequilibrium Can't be misprintized anymore
    iloveaequilibriumbugfixingiloveaequilibriumbugfixingiloveaequilibriumbugfixingiloveaequilibriumbugfixingiloveaequilibriumbugfixingiloveaequilibriumbugfixing
    iloveaequilibriumbugfixingiloveaequilibriumbugfixingiloveaequilibriumbugfixingiloveaequilibriumbugfixingiloveaequilibriumbugfixingiloveaequilibriumbugfixing
    iloveaequilibriumbugfixingiloveaequilibriumbugfixingiloveaequilibriumbugfixingiloveaequilibriumbugfixingiloveaequilibriumbugfixingiloveaequilibriumbugfixing
    iloveaequilibriumbugfixingiloveaequilibriumbugfixingiloveaequilibriumbugfixingiloveaequilibriumbugfixingiloveaequilibriumbugfixingiloveaequilibriumbugfixing

--- Cryptid.lua
+++ Cryptid.lua
@@ -2034,6 +2034,7 @@ function cry_misprintize(card, override, force_reset, stack)
 		(not force_reset or G.GAME.modifiers.cry_jkr_misprint_mod)
 		and (G.GAME.modifiers.cry_misprint_min or override or card.ability.set == "Joker")
 	then
+		if card.ability.name == "Ace Aequilibrium" then return end
 		if G.GAME.modifiers.cry_jkr_misprint_mod and card.ability.set == "Joker" then
 			if not override then
 				override = {}
