748ae56a559425cd453049984bd65a6df79d89ba
Jevonnissocoolman <jevonnsivels2@gmail.com>
2024-10-12 13:11:31 -0700

    Localization

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -491,7 +491,7 @@ return {
                 text = {
                     "All values on this card",
                     "are {C:dark_edition}randomized{}",
-                    "between {C:attention}X0.1{} and {C:attention}X10{}",
+                    "between {C:attention}X0.1{} and {C:attention}X3{}",
                     "{C:inactive}(If possible){}",
                 },
             },
