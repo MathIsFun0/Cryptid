57e7bd36bbf88ce166336c892381958739793d2b
Jevonnissocoolman <jevonnsivels2@gmail.com>
2024-10-14 21:03:04 -0700

    Make Flush House Jokers match Flush Five
    Flush house >>>>>>>>>>

--- Items/MiscJokers.lua
+++ Items/MiscJokers.lua
@@ -3630,7 +3630,7 @@ local unity = {
 	key = "unity",
 	order = 94,
 	pos = { x = 4, y = 0 },
-	config = { Xmult = 7, type = "Flush House" },
+	config = { Xmult = 9, type = "Flush House" },
 	loc_vars = function(self, info_queue, card)
 		return { vars = { card.ability.x_mult, localize(card.ability.type, "poker_hands") } }
 	end,
@@ -3859,7 +3859,7 @@ local wacky = {
 	key = "wacky",
 	pos = { x = 5, y = 5 },
 	order = 21,
-	config = { t_mult = 25, type = "Flush House" },
+	config = { t_mult = 30, type = "Flush House" },
 	loc_vars = function(self, info_queue, card)
 		return { vars = { card.ability.t_mult, localize(card.ability.type, "poker_hands") } }
 	end,
@@ -4053,7 +4053,7 @@ local subtle = {
 	pos = { x = 5, y = 6 },
 	effect = "Cry Type Chips",
 	order = 29,
-	config = { t_chips = 200, type = "Flush House" },
+	config = { t_chips = 240, type = "Flush House" },
 	loc_vars = function(self, info_queue, card)
 		return { vars = { card.ability.t_chips, localize(card.ability.type, "poker_hands") } }
 	end,
