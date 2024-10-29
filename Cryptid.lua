84c00f46e28a05aa3820f982220e25672bed49bf
Jevonnissocoolman <jevonnsivels2@gmail.com>
2024-10-29 11:11:27 -0700

    Jolly Texture pack by B
    requires Malverk Mod
    TODO: Superposition and Sock and Buskin since I don't think those were changed
    hologram scuffed too, fix that later also

--- Cryptid.lua
+++ Cryptid.lua
@@ -2686,6 +2686,31 @@ SMODS.Sound({
 		return Cryptid_config.Cryptid.exotic_music and cry_has_exotic()
 	end,
 })
+
+--Requires Malverk Mod
+if (SMODS.Mods["malverk"] or {}).can_load then
+	AltTexture({
+    		key = 'jolly_jokers',
+    		set = 'Joker',
+    		path = 'jolly.png',
+    		loc_txt = {
+        		name = 'Jolly Jokers'
+    		}
+	})
+	TexturePack{ -- HD Texture Pack
+    		key = 'jolly_texture',
+    		textures = {
+        		'cry_jolly_jokers',
+    		},
+    		loc_txt = {
+       			name = 'Jolly',
+        		text = {
+            			'Jolly Jokers lmao',
+            			'Art by B'
+        		}
+    		}
+	}
+end
 SMODS.Atlas({
 	key = "modicon",
 	path = "cry_icon.png",
