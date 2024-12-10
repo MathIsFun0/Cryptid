0f045f2f708e2ddad19d941daeef59ab8c8b3965
smg9000 <71861369+smg9000@users.noreply.github.com>
2024-12-10 12:45:12 -0600

    Update d20.lua

--- lib/d20.lua
+++ lib/d20.lua
@@ -3,10 +3,23 @@
 -- Currently this is very empty since D20 hasn't been fully implemented yet, but it should have a lot more later.
 
 --Will be moved to D20 file when that gets added
-function roll_dice(seed, min, max, config)
+function roll_dice(seed, max, config,)
 	local val
 	while not val or (config and config.ignore_value == val) do
-		val = pseudorandom(seed, min, max)
+		val = pseudorandom(seed, 1, max)
 	end
-	return val
+	local hit
+	local miss
+	if val => max then
+	    hit = true
+	elseif val <= 1 then
+	    miss = true
+	else hit = false miss = false
+	end
+	    
+	return {
+	    val,
+	    hit,
+	    miss,
+	    }
 end
