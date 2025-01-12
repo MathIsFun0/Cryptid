7426b889a63fd2fe942ad40146d7fbfc51a03bd6
Jevonnissocoolman <jevonnsivels2@gmail.com>
2025-01-11 22:22:56 -0800

    Combine hooks

--- lib/event.lua
+++ lib/event.lua
@@ -50,11 +50,3 @@ G.UIDEF.shop = function(e)
 	end
 	return ret
 end
-
--- Create G.GAME.events when starting a run, so there's no errors
-local gigo = Game.init_game_object
-function Game:init_game_object()
-	local g = gigo(self)
-	g.events = {}
-	return g
-end
